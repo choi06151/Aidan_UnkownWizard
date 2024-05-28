34c9e4db2897d56508ac1204da22e7a4a4331f25
jwk0495 <jwk0495@naver.com>
2024-05-28 17:53:24 +0900

    탄막 수정중, 지휘봉 잡기

--- Source/MyProject/Public/JWK/BulletHellPattern.h
+++ Source/MyProject/Public/JWK/BulletHellPattern.h
@@ -6,22 +6,15 @@
 UENUM(BlueprintType)
 enum class EPatternType : uint8
 {
-	// 직선
-	Straight			UMETA(DisplayName = "Straight"),
-	// 랜덤 직선
-	RandomStraight		UMETA(DisplayName = "RandomStraight"),
-	// 부채꼴
-	Fan					UMETA(DisplayName = "Fan"),
-	// 원
-	Circle				UMETA(DisplayName = "Circle"),
-	// 렌덤 스프레드
-	RandomSpread		UMETA(DisplayName = "Spread"),
-	// 팔각형
-	Octagon				UMETA(DisplayName = "Octagon"),
-	// 나선형
-	Spiral				UMETA(DisplayName = "Spiral"),
-	// 꽃
-	Flower				UMETA(DisplayName = "Flower")
+	Straight,		// 직선				O
+	RandomStraight,	// 랜덤 직선			O
+	Fan,			// 부채꼴			O
+	Circle,			// 원형				O
+	TrumpetFlower,	// 나팔꽃			O
+	Butterfly,		// 나비				? 이게 왜 나비?
+	Crescent,		// 초승달			X
+	Angel			// 천사				X
+	
 };
 
 USTRUCT(BlueprintType)
@@ -30,48 +23,23 @@ struct FBulletHellPattern
 	GENERATED_BODY()
 
 public:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bullet Pattern")
+	// 탄막의 패턴 타입 종류
 	EPatternType PatternType;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bullet Pattern")
-	float Interval;
+	// 탄막 특성들
+	float Interval;				// 발사 주기
+	int32 NumberOfBullets;		// 한 번에 발사되는 총알의 수
+	float BulletSpeed;			// 전체 탄막 속도
+	float PatternSize;			// 패턴 크기
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bullet Pattern")
-	float PatternSize;
-
-	// 직선 탄막 특성
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bullet Pattern",
-		meta = (EditCondition = "PatternType == EPatternType::Straight"))
-	int32 NumberOfBullets;
-
-	// 웨이브 탄막 특성
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bullet Pattern",
-		meta = (EditCondition = "PatternType == EPatternType::Wave"))
-	float Amplitude;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bullet Pattern",
-		meta = (EditCondition = "PatternType == EPatternType::Wave"))
-	float Frequency;
-
-	// 부채꼴 탄막 특성
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bullet Pattern",
-		meta = (EditCondition = "PatternType == EPatternType::Fan"))
-	float FanAngle;
-
-	// 전체 탄막 특성
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Bullet Pattern",
-		meta = (EditCondition = "PatternType == All"))
-	float BulletSpeed;
-
-
-	FBulletHellPattern()
-		: Interval(1.0f),		// 간격
-		  PatternSize(500.0f),	// 패턴의 크기
-		  Amplitude(0.0f),		// 진폭
-		  Frequency(3.0f),		// 빈도
-		  FanAngle(0.0f),		// 부채꼴 각도
-		  NumberOfBullets(10),	// 총알 수
-		  BulletSpeed(800.0f)	// 총알 속도
-	{
-	}
+	float SpiralSpeed;			// 나선형
+	
+	float Amplitude;			// 웨이브 탄막 특성
+	
+	float FanAngle;				// 부채꼴 탄막 특성
+	
+	float ZigzagWidth;			// 지그재그
+	float ZigzagFrequency;		// 지그재그
+	
+	float CrescentAngle;		// 반달
 };
